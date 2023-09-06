6abfdc18db7fb1b09de88c58af464e343bdf54b4
Mauro Dreissig <mukadr@gmail.com>
2023-09-06 19:10:35 -0300

    Token: Let user specify Whitespace Parser

--- ParseSharp/Parser.Token.cs
+++ ParseSharp/Parser.Token.cs
@@ -3,9 +3,9 @@ namespace ParseSharp
     public static partial class Parser
     {
         public static Parser<(string Value, ParserPosition position)> Token(string token) =>
-            Match(token).Map((value, position) => (value, position)).Skip(Whitespace);
+            Match(token).Map((value, position) => (value, position)).Skip(SkipWhitespace);
 
         public static Parser<(T Value, ParserPosition position)> Token<T>(Parser<T> tokenParser) =>
-            tokenParser.Map((value, position) => (value, position)).Skip(Whitespace);
+            tokenParser.Map((value, position) => (value, position)).Skip(SkipWhitespace);
     }
 }
--- ParseSharp/Parser.Whitespace.cs
+++ ParseSharp/Parser.Whitespace.cs
@@ -3,6 +3,8 @@ namespace ParseSharp
     public static partial class Parser
     {
         public static Parser<string> Whitespace { get; } =
-            ZeroOrMore(Match(' ').Or(Match('\t')).Or(Match('\r')).Or(Match('\n')));
+            Match(' ').Or(Match('\t')).Or(Match('\r')).Or(Match('\n'));
+
+        public static Parser<string> SkipWhitespace { get; set; } = ZeroOrMore(Whitespace);
     }
 }
